317a6e16758d2c783c7518a91cea3c459b406e66
Expersso <expersso5@gmail.com>
2016-02-20 17:30:57 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bechdel',
+      author='Eric Persson',
+      author_email='expersso5@gmail.com',
+      version=0.1,
+      license='CC0',
+      description='Retrieve the Bechdel Score of Movies',
+      url='https://github.com/expersson/bechdel')
